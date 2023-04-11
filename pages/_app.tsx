3cb25237d6a598c13985a168d4410945483a06d0
Pavel Kalugin <p.kalugin@paralect.com>
2023-04-11 23:24:48 +0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script async src="https://cdn.splitbee.io/sb.js" />
+    </>
+  );
 }
