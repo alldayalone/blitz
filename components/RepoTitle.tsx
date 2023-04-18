e71fbff3c88a820db620601c8881f3704a66d5c1
Pavel Kalugin <p.kalugin@paralect.com>
2023-04-18 10:12:45 +0300

    add ask ai button

--- components/RepoTitle.tsx
+++ components/RepoTitle.tsx
@@ -1,4 +1,5 @@
 import { useRepo } from "@/stores/repo";
+import { AskAiButton } from "./askAiButton";
 
 export function RepoTitle() {
   const repo = useRepo();
@@ -9,15 +10,19 @@ export function RepoTitle() {
 
   return (
     <div className="my-5">
-      <h1 className="text-3xl">
-        {repo.owner.login}/{repo.name}
-      </h1>
+      <div className="flex items-start mb-4">
+        <h1 className="flex-grow text-3xl">
+          {repo.owner.login}/{repo.name}
+        </h1>
+        <AskAiButton />
+      </div>
+     
       <div className="flex items-end gap-3">
         <p className="flex-grow">{repo.description}</p>
-        <a className="bg-white p-2" data-splitbee-event="Sponsor" href={`https://github.com/sponsors/${repo.owner.login}`} target="_blank">
+        <a className="flex-shrink-0 bg-white p-2" data-splitbee-event="Sponsor" href={`https://github.com/sponsors/${repo.owner.login}`} target="_blank">
           ❤️ Sponsor
         </a>
-        <a className="bg-white p-2" data-splitbee-event="Propose" href={`https://github.com/${repo.owner.login}/${repo.name}/issues/new`} target="_blank">Propose</a>
+        <a className="flex-shrink-0 bg-white p-2" data-splitbee-event="Propose" href={`https://github.com/${repo.owner.login}/${repo.name}/issues/new`} target="_blank">Propose</a>
       </div>
     </div>
   );  
---
+++ components/askAiButton.tsx
@@ -0,0 +1,55 @@
+import { useRepo } from "@/stores/repo";
+import octokit from "@/utils/octokit";
+import { useCallback, useState } from "react"
+import styles from './askAiButton.module.css';
+
+export function AskAiButton() {
+  const repo = useRepo();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClickHandler = useCallback(async () => {
+    if (!repo) return;
+
+    setIsLoading(true);
+    try {
+      const readme = await fetch(`https://raw.githubusercontent.com/${repo.full_name}/main/README.md`).then(res => res.text());
+      const issues = await octokit.rest.issues.listForRepo({
+        owner: repo.owner.login,
+        repo: repo.name,
+        state: 'open',
+      });
+      const roadmap = issues.data.map((issue) => '#' + issue.number + ' ' + issue.title).join('\n');
+
+      const res = await fetch('https://pasha.npkn.net/openai', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          repoName: repo.name,
+          repoHeadline: repo.description,
+          repoDescription: readme,
+          roadmap,        
+        }),
+      });
+
+     
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error('Failed to fetch: ' + JSON.stringify(data));
+      }
+
+      alert(data.suggestions);
+    } catch (error) {
+      alert('ERROR\n' + error);
+    } 
+    
+    setIsLoading(false);
+  }, [repo]);
+
+  return (
+    <button disabled={isLoading} className={`${styles.btn}`} onClick={onClickHandler}>Ask AI{isLoading ? ' ...' : ''}</button>
+  )
+}
