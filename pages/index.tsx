653fa328984ef91f9e98119eb7eb275afc80a274
Pavel Kalugin <p.kalugin@paralect.com>
2023-04-28 20:57:34 +0300

    feat: add author photo on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link';
 import splitbee from "@splitbee/web";
 import { Layout } from '@/components/Layout';
 import Button from "@/components/Button";
+import pashaPhoto from '@/public/pasha_photo.png';
+import Image from "next/image";
 
 const BuilderOnboarding = () => {  
   const router = useRouter();
 
   return (
     <Layout>
-      <div className="flex flex-col gap-5">
-        <h1 className="text-3xl mb-5">setup your public roadmap in 5 mins 😏</h1>
+      <div className="flex flex-col gap-5 relative">
+        <h1 className="text-3xl mt-16 max-xs:mt-0 mb-5">setup your public roadmap in 5 mins 😏</h1>
         <p className="text-muted">do you have a public github repo? just paste a link!</p>
-        <div className="flex flex-col gap-5 mb-12">
+        <div className="flex flex-col gap-5 mb-12 max-w-[28rem]">
           <input className="bg-darkslategray placeholder-[#4c4f6b] py-3 px-4 rounded" type="text" onChange={(e) => {
             const link = e.target.value;
             const repo = link.split('/').slice(-2).join('/');
@@ -26,7 +28,9 @@ const BuilderOnboarding = () => {
           <Button as="a" href="https://github.com/apps/blitzmap" target="_blank" rel="noopener noreferrer" data-splitbee-event="install app" >install gh app</Button>
         </div>
         
-        <p className="text-muted">just chilling? check out blitz roadmap → <Link data-splitbee-event="click roadmap" className='text-blue-600' href="/r?repo=theoberton/blitz">roadmap ( ^ ͜ʖ ^ )</Link></p>
+        <p className="text-muted mb-12">just chilling? check out blitz roadmap → <Link data-splitbee-event="click roadmap" className='text-blue-600' href="/r?repo=theoberton/blitz">roadmap ( ^ ͜ʖ ^ )</Link></p>
+
+        <Image width="150" src={pashaPhoto} alt="author photo" className="absolute bottom-[-120px] left-1/2 translate-x-[-75px] sm:bottom-0 sm:left-full sm:translate-x-[-150px]" />
       </div>
     </Layout>
   )
